379ca26a0a02093f25a49f0f5c93b7f766fa85e1
wanghengxian <jmch03@126.com>
2023-10-23 16:22:34 +0800

    init

--- src/api/report.js
+++
@@ -1,6 +0,0 @@
-import axios from './index'
-
-// 获取非上市企业图表数据
-export const getNonListedCompanyChart = params => {
-  return axios.get('/api/upload-enterprises/industry-model-result', { params })
-}
